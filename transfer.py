86d44c417ca8d72310494815f5e1d84e4d83b0df
Makhnev Konstantin <mah.kons@gmail.com>
2020-11-09 23:59:19 +0300

    almost ok

--- transfer.py
+++ transfer.py
@@ -15,9 +15,9 @@ def create_parser():
     parser.add_argument("--model", type=str, default="vgg19", required=False, choices=["vgg11", "vgg13", "vgg16", "vgg19"])
     parser.add_argument("--content-image", type=str, default="./data/train.png", required=False)
     parser.add_argument("--style-image", type=str, default="./data/starry-night.jpg", required=False)
+    parser.add_argument("--output-image", type=str, default="./generated/tmp.jpg", required=False)
     parser.add_argument("--device", type=str, default="cpu", required=False, choices=["cpu", "cuda"])
     return parser 
-
 
 
 def load_model(model_str):
@@ -31,7 +31,6 @@ def load_model(model_str):
         return torchvision.models.vgg19(pretrained=True)
     else:
         assert(False)
-
 
 _NORMALIZE_MEAN = torch.tensor([0.485, 0.456, 0.406])
 _NORMALIZE_STD = torch.tensor([0.229, 0.224, 0.225])
@@ -50,13 +49,16 @@ def load_image(image_path, to_size, device):
 
 def imshow(image):
     image = (image * _NORMALIZE_STD[:, None, None].to(image.device)) + _NORMALIZE_MEAN[:, None, None].to(image.device)
+    print(image)
+    image.clamp_(0, 1)
     image = torchvision.transforms.ToPILImage()(image.cpu())
     plt.imshow(image)
-    return plt.show()
+    plt.show()
 
-
-
-
+def imsave(image, path):
+    image = (image * _NORMALIZE_STD[:, None, None].to(image.device)) + _NORMALIZE_MEAN[:, None, None].to(image.device)
+    image = torchvision.transforms.ToPILImage()(image.cpu())
+    image.save(path)
 
 
 # features
@@ -72,6 +74,7 @@ def extract_conv_features(model, image):
         elif isinstance(layer, nn.ReLU):
             image = layer(image)
             features.append((i, j, image.clone().squeeze(0)))
+            j += 1
         elif isinstance(layer, nn.MaxPool2d):
             image = layer(image)
 
@@ -91,8 +94,8 @@ def choose_features(features):
 
 
 def get_gram_matrix(image):
-    image = image.reshape(image.shape[0], -1).mean(dim=1, keepdims=True)
-    return image @ image.T
+    image = image.reshape(image.shape[0], -1)
+    return (image @ image.T) / image.shape[1] ** 2
 
 def calc_loss(model, image, content_image, style_image):
     image_features = choose_features(extract_conv_features(model, image))
@@ -108,26 +111,27 @@ def calc_loss(model, image, content_image, style_image):
         gram_style_image = get_gram_matrix(style_image_f)
         style_loss += F.mse_loss(gram_image, gram_style_image)
         
-    return content_loss * CONTENT_COEFF + style_loss * STYLE_COEFF
+    return content_loss, style_loss
 
 
 ITER = 100
-LR = 1
+LR = 1e-1
 CONTENT_COEFF = 1
 STYLE_COEFF = 0
 
 def transfer(model, content_image, style_image, device):
-    image = torch.rand(content_image.shape, device=device)
+    image = torch.rand(content_image.shape, requires_grad=True, device=device)
     optimizer = torch.optim.Adam((image,), lr=LR)
 
-    for i in tqdm(range(ITER)):
-        loss = calc_loss(model, image, content_image, style_image)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        image = image.clamp(0, 1)
-        
+    pbar = tqdm(range(ITER))
+    for i in pbar:
+        content_loss, style_loss = calc_loss(model, image, content_image, style_image)
+        pbar.write("content_loss: {}, style_loss: {}".format(content_loss * CONTENT_COEFF, style_loss * STYLE_COEFF))
+        loss = content_loss * CONTENT_COEFF + style_loss * STYLE_COEFF
+        with torch.no_grad():
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
     return image
 
@@ -142,3 +146,4 @@ if __name__ == "__main__":
     new_image = transfer(model, content_image, style_image, device)
 
     imshow(torchvision.utils.make_grid([style_image, content_image, new_image]))
+    imsave(new_image, args.output_image)
